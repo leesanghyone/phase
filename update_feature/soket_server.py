b036275e0899642831476bb2551f044fd74b34dc
leesanghyone <lsh920910@gmail.com>
2023-04-29 19:57:17 +0900

    첫번째 테스트 완성본

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -1,6 +1,6 @@
 import socket,json,threading,time,sys
 import socket,json,threading,time,sys
-from Coupang_work_fungtion import *
+# from Coupang_work_fungtion import *
 
 
 def initstater():
@@ -11,21 +11,33 @@ def initstater():
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.bind(("127.0.0.1",12000))
     sock.listen()
+    print("서버를 창설했습니다.")
     c_sock,addr=sock.accept()
     print("클라이언트가 들어왔다.")
     return c_sock
 
 def receiver(c_sock):
-
-    #1.작업방식을 설정하는 데이터.
-    작업방식=c_sock.recv(1024).decode("utf-8")
-    if 작업방식=="서버정보업데이트":
-        print("서버정보 업데이트를 클라이언트가 요청했습니다")
-    elif 작업방식=="가구매작업":
-        print("가구매작업을 데이터를 받았습니다.")
+    while True:
+        #1.작업방식을 설정하는 데이터.
+        작업방식=c_sock.recv(1024).decode("utf-8")
+        if 작업방식=="서버정보업데이트":
+            print("서버정보 업데이트를 클라이언트가 요청했습니다")
+        elif 작업방식=="가구매작업":
+            print("가구매작업을 데이터를 받았습니다.")
 
 def worker(c_sock):
     pass
 
 def timeamanager():
     pass
+
+def socket_start():
+    c_sock=initstater()
+    threading.Thread(target=receiver,args=(c_sock,)).start()
+    threading.Thread(target=worker,args=(c_sock,)).start()
+
+
+
+if __name__ == '__main__':
+    socket_start()
+
--- update_feature/sokey_client.py
+++ update_feature/sokey_client.py
@@ -1,10 +1,10 @@
 import socket,json,threading,time
-from excel import *
+# from excel import *
 
 
 def initstater(ip=str,port=int):
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect(ip,port)
+    sock.connect((ip,port))
     return sock
     
 
@@ -22,7 +22,23 @@ def receiver(sock):
             sock.sendall(json.dumps(보낼데이터).encode("utf-8"))
 
         elif 작업종류설정=="서버정보업데이트":
-            pass 
+            print("서버정보 업데이트 데이터를 받았습니다.")
+             
         else:
             print("작업종류설정이 잘못되었습니다.")
-        
+
+
+def sender(sock):
+    while True:
+        작업방식=input("작업방식을 설정해주세요(가구매작업,서버정보업데이트):)")
+        sock.sendall(작업방식.encode("utf-8"))
+
+
+def soket_start(ip,port):
+    sock=initstater(ip,port)
+    threading.Thread(target=receiver,args=(sock,)).start()
+    threading.Thread(target=sender,args=(sock,)).start()
+
+
+if __name__ == '__main__':
+    soket_start("127.0.0.1",12000)
