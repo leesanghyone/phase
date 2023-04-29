c670cb758d270b872415ddeaa1c1e70984031e6b
leesanghyone <lsh920910@gmail.com>
2023-04-29 19:21:49 +0900

    잘못된점 다시 수정함

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -4,6 +4,10 @@ from Coupang_work_fungtion import *
 
 
 def initstater():
+    global waitlist,workerlist
+    #1. 준비재료
+    waitlist=[]
+    workerlist=[]
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.bind(("127.0.0.1",12000))
     sock.listen()
@@ -12,16 +16,16 @@ def initstater():
     return c_sock
 
 def receiver(c_sock):
+
     #1.작업방식을 설정하는 데이터.
-    if c_sock.recv(1024).decode("utf-8")=="서버정보업데이트":
+    작업방식=c_sock.recv(1024).decode("utf-8")
+    if 작업방식=="서버정보업데이트":
         print("서버정보 업데이트를 클라이언트가 요청했습니다")
-    elif c_sock.recv(1024).decode("utf-8")=="가구매작업":
+    elif 작업방식=="가구매작업":
         print("가구매작업을 데이터를 받았습니다.")
 
-def sender(c_sock):
-    #2.작업방식에 따라서 데이터를 보내는 데이터.
-    if c_sock.recv(1024).decode("utf-8")=="서버정보업데이트":
-        print("서버정보 업데이트를 클라이언트가 요청했습니다")
-    elif c_sock.recv(1024).decode("utf-8")=="가구매작업":
-        print("가구매작업을 데이터를 받았습니다.")
+def worker(c_sock):
+    pass
+
+def timeamanager():
     pass
