17ad9cf8f452a44b475960b19fd60969dbced70f
Your Name <you@example.com>
2023-04-30 01:49:54 +0900

    완성

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -6,7 +6,7 @@ import socket,json,threading,time,sys
 def initstater(ip=str,port=int):
     global waitlist,workerlist
     #1. 준비재료
-    waitlist=[{"url": "https://copang.com","작업시간" : 100000,"플랫폼" : "쿠팡"},{"url": "https://naver.com","작업시간" : 20000,"플랫폼" : 30000}]
+    waitlist=[{"작업시간": "15:43","플랫폼" : "쿠팡"},{"작업시간": "13:30","플랫폼" : "네이버"}] #1.작업시간, 2.플랫폼
     workerlist=[]
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.bind((ip,port))
@@ -20,21 +20,22 @@ def receiver(c_sock):
     while True:
         #1.작업방식을 설정하는 데이터.
         작업방식=c_sock.recv(1024).decode("utf-8")
+        ###########################################
         if 작업방식=="가구매작업":
             print("가구매작업을 데이터를 받았습니다.")
             작업데이터=json.loads(c_sock.recv(1024).decode("utf-8"))
             waitlist.append(작업데이터)
             print(작업데이터["url"],작업데이터["최대가격"])
-            
+        ###########################################    
         elif 작업방식=="서버정보업데이트":
             print("서버정보 업데이트를 클라이언트가 요청했습니다")
             
-            #1. 클라이언트에게 작업방식을 보내준다.
+            #2. 클라이언트에게 작업방식을 보내준다.
             c_sock.sendall("서버정보업데이트".encode("utf-8"))
             
-            #서버일감 정보를 보내준다.
+            #3.서버일감 정보를 보내준다.
             for work in waitlist:
-                sendwork=[waitlist.index(work),work["url"],work["작업시간"],work["플랫폼"],waitlist.index(work)] #1.인덱스, 2. url, 3. 작업시간, 4. 플랫폼 , 5.고유인덱스
+                sendwork=[waitlist.index(work),work["작업시간"],work["플랫폼"],waitlist.index(work)] #1.인덱스, 2.작업시간, 3.플랫폼 ,4.고유인덱스
                 c_sock.sendall(json.dumps(sendwork).encode("utf-8"))
             c_sock.sendall("작업끝".encode("utf-8"))
 
@@ -59,7 +60,7 @@ def receiver(c_sock):
                         변경데이터=waitlist[변경인덱스]
                         waitlist[고유인덱스]=변경데이터
                         waitlist[변경인덱스]=고유데이터
-
+                        print("인덱스를 수정했습니다.")
             elif 응답=="수정없음":
                 pass
 
