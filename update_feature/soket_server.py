9eb6641aee6018d07f8e7eb053e5275c0166dfcc
leesanghyone <lsh920910@gmail.com>
2023-04-29 20:27:56 +0900

    1차모의테스트:가구매자업데이터까지 전송이 가능함

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -3,13 +3,13 @@ import socket,json,threading,time,sys
 # from Coupang_work_fungtion import *
 
 
-def initstater():
+def initstater(ip=str,port=int):
     global waitlist,workerlist
     #1. 준비재료
     waitlist=[]
     workerlist=[]
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.bind(("127.0.0.1",12000))
+    sock.bind((ip,port))
     sock.listen()
     print("서버를 창설했습니다.")
     c_sock,addr=sock.accept()
@@ -22,8 +22,13 @@ def receiver(c_sock):
         작업방식=c_sock.recv(1024).decode("utf-8")
         if 작업방식=="서버정보업데이트":
             print("서버정보 업데이트를 클라이언트가 요청했습니다")
+        
         elif 작업방식=="가구매작업":
             print("가구매작업을 데이터를 받았습니다.")
+            작업데이터=json.loads(c_sock.recv(1024).decode("utf-8"))
+            print(작업데이터["url"],작업데이터["최대가격"])
+        else:
+            print("작업방식이 잘못되었습니다.")
 
 def worker(c_sock):
     pass
@@ -31,13 +36,12 @@ def worker(c_sock):
 def timeamanager():
     pass
 
-def socket_start():
-    c_sock=initstater()
+def socket_start(ip=str,port=int):
+    c_sock=initstater(ip,port)
     threading.Thread(target=receiver,args=(c_sock,)).start()
     threading.Thread(target=worker,args=(c_sock,)).start()
 
 
-
 if __name__ == '__main__':
-    socket_start()
+    socket_start("127.0.0.1",12000)
 
--- update_feature/sokey_client.py
+++ update_feature/sokey_client.py
@@ -14,13 +14,6 @@ def receiver(sock):
         if 작업종류설정=="가구매작업":
             print("가구매작업 결과물 데이터를 받았습니다.")
 
-            보낼데이터={ 
-                "url": "Https://copang",
-                "최대가격" : 100000,
-                "최소가격" : 10000,
-            }
-            sock.sendall(json.dumps(보낼데이터).encode("utf-8"))
-
         elif 작업종류설정=="서버정보업데이트":
             print("서버정보 업데이트 데이터를 받았습니다.")
              
@@ -28,17 +21,33 @@ def receiver(sock):
             print("작업종류설정이 잘못되었습니다.")
 
 
-def sender(sock):
-    while True:
-        작업방식=input("작업방식을 설정해주세요(가구매작업,서버정보업데이트):)")
-        sock.sendall(작업방식.encode("utf-8"))
-
+def socket_sender(sock,작업방식,가구매작업데이터):
+    #작업방식 데이터 보내기.
+    sock.sendall(작업방식.encode("utf-8"))
+    #1.가구매 작업 데이터 보내기.
+    if 작업방식 == "가구매작업":
+        print("가구매 작업 데이터를 보냅니다.")
+        sock.sendall(json.dumps(가구매작업데이터).encode("utf-8"))
+    #2.서버정보업데이트 정보 요청하기.
+    if 작업방식 == "서버정보업데이트":
+        print("서버정보업데이트 정보 요청을 보냅니다.")
 
 def soket_start(ip,port):
     sock=initstater(ip,port)
     threading.Thread(target=receiver,args=(sock,)).start()
-    threading.Thread(target=sender,args=(sock,)).start()
+    return sock
+    
 
-
+#사용방법이다.
 if __name__ == '__main__':
-    soket_start("127.0.0.1",12000)
+    sock=soket_start("127.0.0.1",12000)
+
+    while True:
+        작업방식=input("작업방식을 설정해주세요:")
+        가구매작업데이터={ 
+            "url": "Https://copang",
+            "최대가격" : 100000,
+            "최소가격" : 10000,
+        }
+        socket_sender(sock,작업방식,가구매작업데이터)
+
