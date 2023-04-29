e85e4b291d21634e6c60ce295d174eda2a72faf1
leesanghyone <lsh920910@gmail.com>
2023-04-29 19:16:21 +0900

    소켓 기초뼈대

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -3,3 +3,25 @@ import socket,json,threading,time,sys
 from Coupang_work_fungtion import *
 
 
+def initstater():
+    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.bind(("127.0.0.1",12000))
+    sock.listen()
+    c_sock,addr=sock.accept()
+    print("클라이언트가 들어왔다.")
+    return c_sock
+
+def receiver(c_sock):
+    #1.작업방식을 설정하는 데이터.
+    if c_sock.recv(1024).decode("utf-8")=="서버정보업데이트":
+        print("서버정보 업데이트를 클라이언트가 요청했습니다")
+    elif c_sock.recv(1024).decode("utf-8")=="가구매작업":
+        print("가구매작업을 데이터를 받았습니다.")
+
+def sender(c_sock):
+    #2.작업방식에 따라서 데이터를 보내는 데이터.
+    if c_sock.recv(1024).decode("utf-8")=="서버정보업데이트":
+        print("서버정보 업데이트를 클라이언트가 요청했습니다")
+    elif c_sock.recv(1024).decode("utf-8")=="가구매작업":
+        print("가구매작업을 데이터를 받았습니다.")
+    pass
--- update_feature/sokey_client.py
+++ update_feature/sokey_client.py
@@ -12,7 +12,14 @@ def receiver(sock):
     while True:
         작업종류설정=sock.recv(1024).decode("utf-8")
         if 작업종류설정=="가구매작업":
-            sock.sendall(json.dumps(["작업종류설정","가구매작업"]).encode("utf-8"))
+            print("가구매작업 결과물 데이터를 받았습니다.")
+
+            보낼데이터={ 
+                "url": "Https://copang",
+                "최대가격" : 100000,
+                "최소가격" : 10000,
+            }
+            sock.sendall(json.dumps(보낼데이터).encode("utf-8"))
 
         elif 작업종류설정=="서버정보업데이트":
             pass 
