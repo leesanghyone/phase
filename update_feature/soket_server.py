98d7113fa5f63832598dfd44b944cf98eb242552
leesanghyone <lsh920910@gmail.com>
2023-04-29 20:53:00 +0900

    서버 일감 쪽을 수정해야 된다

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -6,7 +6,7 @@ import socket,json,threading,time,sys
 def initstater(ip=str,port=int):
     global waitlist,workerlist
     #1. 준비재료
-    waitlist=[]
+    waitlist=[{"url": "https://copang.com","작업시간" : 100000,"플랫폼" : "쿠팡"},{"url": "https://naver.com","작업시간" : 20000,"플랫폼" : 30000}]
     workerlist=[]
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.bind((ip,port))
@@ -20,13 +20,25 @@ def receiver(c_sock):
     while True:
         #1.작업방식을 설정하는 데이터.
         작업방식=c_sock.recv(1024).decode("utf-8")
-        if 작업방식=="서버정보업데이트":
-            print("서버정보 업데이트를 클라이언트가 요청했습니다")
-        
-        elif 작업방식=="가구매작업":
+        if 작업방식=="가구매작업":
             print("가구매작업을 데이터를 받았습니다.")
             작업데이터=json.loads(c_sock.recv(1024).decode("utf-8"))
             print(작업데이터["url"],작업데이터["최대가격"])
+            
+        elif 작업방식=="서버정보업데이트":
+            print("서버정보 업데이트를 클라이언트가 요청했습니다")
+            
+            #1. 클라이언트에게 작업방식을 보내준다.
+            c_sock.sendall("서버정보업데이트".encode("utf-8"))
+            
+            #서버일감 정보를 보내준다.
+            for work in waitlist:
+                c_sock.sendall(json.dumps(work).encode("utf-8"))
+            c_sock.sendall("작업끝".encode("utf-8"))
+            
+
+
+
         else:
             print("작업방식이 잘못되었습니다.")
 
--- update_feature/sokey_client.py
+++ update_feature/sokey_client.py
@@ -16,21 +16,31 @@ def receiver(sock):
 
         elif 작업종류설정=="서버정보업데이트":
             print("서버정보 업데이트 데이터를 받았습니다.")
+            while True:
+                서버일감=sock.recv(1024).decode("utf-8")
+                if 서버일감=="작업끝":
+                    break
+                서버일감=json.loads(서버일감)
+                print(서버일감["url"],서버일감["작업시간"],서버일감["플랫폼"])
+            #서버일감을 어떻게 테이블 위젯으로 구현을 하는가... 이게 좀 빡세다.
              
         else:
             print("작업종류설정이 잘못되었습니다.")
 
 
+
 def socket_sender(sock,작업방식,가구매작업데이터):
     #작업방식 데이터 보내기.
     sock.sendall(작업방식.encode("utf-8"))
+
     #1.가구매 작업 데이터 보내기.
     if 작업방식 == "가구매작업":
         print("가구매 작업 데이터를 보냅니다.")
         sock.sendall(json.dumps(가구매작업데이터).encode("utf-8"))
+
     #2.서버정보업데이트 정보 요청하기.
     if 작업방식 == "서버정보업데이트":
-        print("서버정보업데이트 정보 요청을 보냅니다.")
+        print("서버정보업데이트 정보요청을 보냈습니다..")
 
 def soket_start(ip,port):
     sock=initstater(ip,port)
@@ -41,13 +51,13 @@ def soket_start(ip,port):
 #사용방법이다.
 if __name__ == '__main__':
     sock=soket_start("127.0.0.1",12000)
-
-    while True:
-        작업방식=input("작업방식을 설정해주세요:")
-        가구매작업데이터={ 
-            "url": "Https://copang",
-            "최대가격" : 100000,
-            "최소가격" : 10000,
-        }
-        socket_sender(sock,작업방식,가구매작업데이터)
+    ###보내야 할 데이터를 설정한다.
+    작업방식=input("작업방식을 설정해주세요:")
+    가구매작업데이터={ 
+        "url": "Https://copang",
+        "최대가격" : 100000,
+        "최소가격" : 10000,
+    }
+    socket_sender(sock,작업방식,가구매작업데이터) #데이터를 보낸다.
+       
 
