5f171dafa2895d06613107828cd4e49641aebc20
leesanghyone <lsh920910@gmail.com>
2023-04-30 17:58:13 +0900

    카페에서 일 하기 싫어서 정리해둠

--- update_feature/soket_server.py
+++ update_feature/soket_server.py
@@ -1,12 +1,12 @@
 import socket,json,threading,time,sys
 import socket,json,threading,time,sys
-# from Coupang_work_fungtion import *
+from Coupang_work_fungtion import *
 
 
 def initstater(ip=str,port=int):
     global waitlist,workerlist
     #1. 준비재료
-    waitlist=[{"작업시간": "15:43","플랫폼" : "쿠팡"},{"작업시간": "13:30","플랫폼" : "네이버"}] #1.작업시간, 2.플랫폼
+    waitlist=[{"url": "https://copang.com","작업시간" : 100000,"플랫폼" : "쿠팡"},{"url": "https://naver.com","작업시간" : 20000,"플랫폼" : 30000}]
     workerlist=[]
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.bind((ip,port))
@@ -20,69 +20,31 @@ def receiver(c_sock):
     while True:
         #1.작업방식을 설정하는 데이터.
         작업방식=c_sock.recv(1024).decode("utf-8")
-        ###########################################
         if 작업방식=="가구매작업":
             print("가구매작업을 데이터를 받았습니다.")
             작업데이터=json.loads(c_sock.recv(1024).decode("utf-8"))
             waitlist.append(작업데이터)
             print(작업데이터["url"],작업데이터["최대가격"])
-        ##2.서버정보업데이트
+            
         elif 작업방식=="서버정보업데이트":
             print("서버정보 업데이트를 클라이언트가 요청했습니다")
             
-            #2-1. 클라이언트에게 작업방식을 보내준다.
+            #1. 클라이언트에게 작업방식을 보내준다.
             c_sock.sendall("서버정보업데이트".encode("utf-8"))
             
-            #2-2서버일감 정보를 보내준다.
+            #서버일감 정보를 보내준다.
             for work in waitlist:
-                sendwork=[waitlist.index(work),work["작업시간"],work["플랫폼"],waitlist.index(work)] #1.인덱스, 2.작업시간, 3.플랫폼 ,4.고유인덱스
-                c_sock.sendall(json.dumps(sendwork).encode("utf-8"))
-                time.sleep(1)
+                c_sock.sendall(json.dumps(work).encode("utf-8"))
             c_sock.sendall("작업끝".encode("utf-8"))
-            print("서버정보 업데이트를 완료했습니다.")
-
-            #2-3클라이언측의 응답을 받는다.
-            응답=c_sock.recv(1024).decode("utf-8")
-
-            if 응답=="수정있음":
-                while True:
-                    수정데이터=json.loads(c_sock.recv(1024).decode("utf-8"))
-                    if 수정데이터=="수정끝":
-                        break
-                    waitlist[수정데이터[4]]["url"]=수정데이터[1]
-                    waitlist[수정데이터[4]]["작업시간"]=수정데이터[2]
-                    waitlist[수정데이터[4]]["플랫폼"]=수정데이터[3]
-                    print("수정완료")
-
-            #2-3.인덱스를 수정사항이 있다면, 수정한다.
-                    if 수정데이터[0]!=수정데이터[4]:
-                        고유인덱스=수정데이터[4]
-                        고유데이터=waitlist[고유인덱스]
-                        변경인덱스=수정데이터[0]
-                        변경데이터=waitlist[변경인덱스]
-                        waitlist[고유인덱스]=변경데이터
-                        waitlist[변경인덱스]=고유데이터
-                        print("인덱스를 수정했습니다.")
-            #2-4. 인덱스를 삭제한 경우.
-            elif 응답=="수정없음":
-                print("수정없음")
             
         else:
             print("작업방식이 잘못되었습니다.")
 
-def timemanager():
-    while True:
-        쓰레드락=threading.Lock()
-        with 쓰레드락:
-            예약시간=waitlist[0]["작업시간"]
-        while True:
-            time.time()-time.mktime(time.strptime(예약시간,"%H:%M"))
-            time.sleep(1)
-
-
 def worker(c_sock):
     pass
 
+def timeamanager():
+    pass
 
 def socket_start(ip=str,port=int):
     c_sock=initstater(ip,port)
